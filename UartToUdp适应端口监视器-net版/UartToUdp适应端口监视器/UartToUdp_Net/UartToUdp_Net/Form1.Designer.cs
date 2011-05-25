9efc8bd4b75d062e349937079a4acbf28f78a2b8
chengxianming1981@gmail.com <chengxianming1981@gmail.com@5d2555b2-77f6-cb3c-1dbd-c1bb7278d601>
2011-05-25 08:33:37 +0000

    TEST .net实现串口接收数据通过UDP发送
    git-svn-id: https://cxm-hitech-matrix428.googlecode.com/svn/trunk@24 5d2555b2-77f6-cb3c-1dbd-c1bb7278d601

---
+++ UartToUdp适应端口监视器-net版/UartToUdp适应端口监视器/UartToUdp_Net/UartToUdp_Net/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace UartToUdp_Net
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.axMSComm1 = new AxMSCommLib.AxMSComm();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.dataSet1 = new System.Data.DataSet();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.axMSComm1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "端口设置";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "波特率设置";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "端口1",
+            "端口2",
+            "端口3",
+            "端口4",
+            "端口5",
+            "端口6",
+            "端口7",
+            "端口8",
+            "端口9"});
+            this.comboBox1.Location = new System.Drawing.Point(121, 19);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 20);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Text = "端口1";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "9600",
+            "38400",
+            "57600",
+            "115200"});
+            this.comboBox2.Location = new System.Drawing.Point(121, 78);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(100, 20);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.Text = "115200";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Location = new System.Drawing.Point(8, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 33);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "打开串口";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // axMSComm1
+            // 
+            this.axMSComm1.Enabled = true;
+            this.axMSComm1.Location = new System.Drawing.Point(117, 180);
+            this.axMSComm1.Name = "axMSComm1";
+            this.axMSComm1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMSComm1.OcxState")));
+            this.axMSComm1.Size = new System.Drawing.Size(38, 38);
+            this.axMSComm1.TabIndex = 5;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5;
+            this.timer1.Tick += new System.EventHandler(this.OnTimer);
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.comboBox1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.axMSComm1, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.comboBox2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(228, 239);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(228, 239);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.axMSComm1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private AxMSCommLib.AxMSComm axMSComm1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Data.DataSet dataSet1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
+
---
+++ UartToUdp适应端口监视器-net版/UartToUdp适应端口监视器/UartToUdp_Net/UartToUdp_Net/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+
+
+
+namespace UartToUdp_Net
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private uint m_icount = 0;
+        private byte[] uart_buf = new byte[256];
+        private byte[] udp_buf = new byte[128];
+        private UdpClient udpReceive;
+        private UdpClient udpSend;
+        //创建一个Thread类
+        private Thread thread1;
+        // 在本机指定的端口发送
+        private IPEndPoint remoteIPEndPointSend = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 0x9001);
+        // 在本机指定的端口接收
+        private IPEndPoint remoteIPEndPointRec = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 0X9002);
+
+        private IPEndPoint iep = new IPEndPoint(IPAddress.Any, 0);
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 先关闭已经打开的端口
+            if (axMSComm1.PortOpen)
+            {
+                axMSComm1.PortOpen = false;
+            }
+            string sPort;
+            string sBaud;
+            short iPort = 0;
+            sPort = comboBox1.Text;
+            if (sPort == "端口1")
+            {
+                iPort = 1;
+            }
+            else if (sPort == "端口2")
+            {
+                iPort = 2;
+            }
+            else if (sPort == "端口3")
+            {
+                iPort = 3;
+            }
+            else if (sPort == "端口4")
+            {
+                iPort = 4;
+            }
+            else if (sPort == "端口5")
+            {
+                iPort = 5;
+            }
+            else if (sPort == "端口6")
+            {
+                iPort = 6;
+            }
+            else if (sPort == "端口7")
+            {
+                iPort = 7;
+            }
+            else if (sPort == "端口8")
+            {
+                iPort = 8;
+            }
+            else if (sPort == "端口9")
+            {
+                iPort = 9;
+            }
+            sBaud = comboBox2.Text;
+            axMSComm1.CommPort = iPort;
+
+            axMSComm1.Settings = sBaud + ",n,8,1";
+
+            // 成功打开端口
+            if (!axMSComm1.PortOpen)
+            {
+                // 打开串口
+                axMSComm1.PortOpen = true;
+                // 清空接收缓冲区
+                axMSComm1.InBufferCount = 0;
+                // 清空发送缓冲区
+                axMSComm1.OutBufferCount = 0;
+                MessageBox.Show("Open Serial Port Successful!");
+                // 打开定时器1
+                timer1.Enabled = true;
+                timer1.Start();
+                // 启动线程
+                thread1.Start();
+            }
+        }
+        private void OnSendFromUDP(byte[] TXDATA, int uiCount)
+        {
+            udpSend.Send(TXDATA, uiCount, remoteIPEndPointSend);    
+        }
+        private void OnTimer(object sender, EventArgs e)
+        {
+            object oResponse;
+            short len;
+            byte[] cRecBuf = new byte[256];
+            len = axMSComm1.InBufferCount; //接收到的字符数目
+            if (len > 0)
+            {
+                // 设置需要读取的输入缓冲区大小，为了不丢帧
+                axMSComm1.InputLen = len;
+                oResponse = axMSComm1.Input; //read
+
+                cRecBuf = (byte[])oResponse;
+
+                if (len > 1024)
+                {
+                    MessageBox.Show("Receive data from Uart leaked!");
+                    return;
+                }
+                for (int i = 0; i < len; i++)
+                {
+                    switch (m_icount)
+                    {
+                        case 0:
+                            // byte.Parse(str.Substring(i, 1)) == 0x11
+                            if (cRecBuf[i] == 0x11)
+                            {
+                                uart_buf[0] = cRecBuf[i];
+                                m_icount++;
+                                break;
+                            }
+                            else
+                            {
+                                m_icount = 0;
+                                break;
+                            }
+
+                        case 1:
+                            if (cRecBuf[i] == 0x22)
+                            {
+                                uart_buf[1] = cRecBuf[i];
+                                m_icount++;
+                                break;
+                            }
+                            else
+                            {
+                                m_icount = 0;
+                                break;
+                            }
+                        case 2:
+                            if (cRecBuf[i] == 0x33)
+                            {
+                                uart_buf[2] = cRecBuf[i];
+                                m_icount++;
+                                break;
+                            }
+                            else
+                            {
+                                m_icount = 0;
+                                break;
+                            }
+                        case 3:
+                            if (cRecBuf[i] == 0x44)
+                            {
+                                uart_buf[3] = cRecBuf[i];
+                                m_icount++;
+                                break;
+                            }
+                            else
+                            {
+                                m_icount = 0;
+                                break;
+                            }
+                        default:
+                            uart_buf[m_icount] = cRecBuf[i];
+                            m_icount++;
+                            if (m_icount == 256)
+                            {
+                                m_icount = 0;
+                                OnSendFromUDP(uart_buf, 256);
+                            }
+                            break;
+                    }
+                }
+            }
+        }
+        private void ReceiveMessage()
+        {
+
+            while (true)
+            {
+                // ref表示引用类型的 IPPoint实例接收消息
+                udp_buf = udpReceive.Receive(ref iep);
+                axMSComm1.Output = udp_buf;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            udpSend = new UdpClient();
+            udpReceive = new UdpClient(remoteIPEndPointRec);
+
+            // 初始化该线程并指定线程执行时要调用的方法
+            thread1 = new Thread(new ThreadStart(ReceiveMessage));
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            timer1.Enabled = false;
+            udpSend.Close();
+            udpReceive.Close();
+            thread1.Abort();
+        }
+    }
+}
