31127ed8463efbfb8b3a44af7af37b600a358e91
chengxianming1981@gmail.com <chengxianming1981@gmail.com@5d2555b2-77f6-cb3c-1dbd-c1bb7278d601>
2011-05-26 02:54:47 +0000

    TEST BCG做界面5-导入数据到列表(BCG)
    git-svn-id: https://cxm-hitech-matrix428.googlecode.com/svn/trunk@34 5d2555b2-77f6-cb3c-1dbd-c1bb7278d601

---
+++ Test5/Test5/WorkSpaceBar.h
@@ -0,0 +1,46 @@
+// workspacebar.h : interface of the CWorkSpaceBar class
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+class CWorkSpaceBar : public CBCGPDockingControlBar
+{
+public:
+	CWorkSpaceBar();
+
+// Attributes
+protected:
+	CBCGPGridCtrl* m_pGridView;
+
+// Operations
+public:
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CWorkSpaceBar)
+	//}}AFX_VIRTUAL
+
+	// Implementation
+public:
+	virtual ~CWorkSpaceBar();
+	virtual bool SetGridHead();
+
+	// Generated message map functions
+protected:
+	//{{AFX_MSG(CWorkSpaceBar)
+	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
+	afx_msg void OnSize(UINT nType, int cx, int cy);
+	afx_msg void OnDestroy();
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+public:
+	void LoadMembers(char* csmembers[]);
+	void LoadMember(char csmember[]);
+};
+
+/////////////////////////////////////////////////////////////////////////////
