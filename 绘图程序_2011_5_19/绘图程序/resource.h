7843bdca754b33aa1ecf13d70e57d05bf5d3c7ba
chengxianming1981@gmail.com <chengxianming1981@gmail.com@5d2555b2-77f6-cb3c-1dbd-c1bb7278d601>
2011-05-26 07:52:40 +0000

    Work 地震绘图程序 2011_5_19
    git-svn-id: https://cxm-hitech-matrix428.googlecode.com/svn/trunk@64 5d2555b2-77f6-cb3c-1dbd-c1bb7278d601

---
+++ 绘图程序_2011_5_19/绘图程序/resource.h
@@ -0,0 +1,34 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by »æÍ¼³ÌÐò.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MY_DIALOG                   102
+#define IDR_MAINFRAME                   128
+#define IDI_ZoomOutPB                   129
+#define IDI_PointerPB                   130
+#define IDI_XYZoomPB                    131
+#define IDI_XZoomPB                     132
+#define IDI_ZoomInPB                    133
+#define IDC_                            1000
+#define IDC_ChartViewer                 1000
+#define IDC_PointerPB                   1005
+#define IDC_ZoomInPB                    1006
+#define IDC_ZoomOutPB                   1007
+#define IDC_XZoomPB                     1008
+#define IDC_XYZoomPB                    1009
+#define IDC_VScrollBar                  1010
+#define IDC_HScrollBar                  1011
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        134
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1012
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
