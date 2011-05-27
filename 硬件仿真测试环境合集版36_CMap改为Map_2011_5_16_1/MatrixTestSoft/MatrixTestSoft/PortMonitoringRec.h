901cd3fc8f2cb2399ede3d51c294ead1390d8f93
chengxianming1981@gmail.com <chengxianming1981@gmail.com@5d2555b2-77f6-cb3c-1dbd-c1bb7278d601>
2011-05-27 06:08:51 +0000

    Work 硬件仿真测试环境合集版36_CMap改为Map_2011_5_16_1
    git-svn-id: https://cxm-hitech-matrix428.googlecode.com/svn/trunk@107 5d2555b2-77f6-cb3c-1dbd-c1bb7278d601

---
+++ 硬件仿真测试环境合集版36_CMap改为Map_2011_5_16_1/MatrixTestSoft/MatrixTestSoft/PortMonitoringRec.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "PortMonitoringSendThread.h"
+#include <afxmt.h>
+// CPortMonitoringRec 对话框
+
+class CPortMonitoringRec : public CDialog
+{
+	DECLARE_DYNAMIC(CPortMonitoringRec)
+
+public:
+	CPortMonitoringRec(CWnd* pParent = NULL);   // 标准构造函数
+	virtual ~CPortMonitoringRec();
+
+// 对话框数据
+	enum { IDD = IDD_DIALOG_PORTMONITORING_REC };
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+	DECLARE_MESSAGE_MAP()
+protected:
+	// 定义临界区变量
+	CCriticalSection m_Sec_PortMonitorRec;
+public:
+	// 线程类指针
+	CPortMonitoringSendThread* m_pSendThread;
+public:
+	afx_msg void OnTimer(UINT_PTR nIDEvent);
+};
