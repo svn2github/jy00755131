abc97ad1bf8549789f0c866d95a4b175ded37c71
chengxianming1981@gmail.com <chengxianming1981@gmail.com@5d2555b2-77f6-cb3c-1dbd-c1bb7278d601>
2011-05-27 01:12:59 +0000

    Debug 硬件仿真测试环境合集版14_2_采样数据改为72个
    git-svn-id: https://cxm-hitech-matrix428.googlecode.com/svn/trunk@75 5d2555b2-77f6-cb3c-1dbd-c1bb7278d601

---
+++ 硬件仿真测试环境合集版14_采样数据改为72个_调试用2/MatrixTestSoft/MatrixTestSoft/XMLDOMTool.cpp
@@ -0,0 +1,90 @@
+#include "StdAfx.h"
+#include "XMLDOMTool.h"
+
+CXMLDOMTool::CXMLDOMTool(void)
+{
+}
+
+CXMLDOMTool::~CXMLDOMTool(void)
+{
+}
+
+// 解析属性，返回字符串
+CString CXMLDOMTool::GetElementAttributeString(CXMLDOMElement* pElement, CString strKey)
+{
+	COleVariant oVariant = pElement->getAttribute(strKey);
+	CString strData = oVariant;
+	return strData;
+}
+
+// 解析属性，返回整数
+int CXMLDOMTool::GetElementAttributeInt(CXMLDOMElement* pElement, CString strKey)
+{
+	COleVariant oVariant = pElement->getAttribute(strKey);
+	oVariant.ChangeType(VT_INT);
+	int iData = oVariant.intVal;
+	return iData;
+}
+
+// 解析属性，返回无符号整数
+unsigned int CXMLDOMTool::GetElementAttributeUnsignedInt(CXMLDOMElement* pElement, CString strKey)
+{
+	COleVariant oVariant = pElement->getAttribute(strKey);
+	oVariant.ChangeType(VT_UINT);
+	unsigned int uiData = oVariant.uintVal;
+	return uiData;
+}
+
+// 解析属性，返回单精度浮点数
+float CXMLDOMTool::GetElementAttributeFloat(CXMLDOMElement* pElement, CString strKey)
+{
+	COleVariant oVariant = pElement->getAttribute(strKey);
+	oVariant.ChangeType(VT_R4);
+	float fData = oVariant.fltVal;
+	return fData;
+}
+
+// 解析属性，返回双精度浮点数
+double CXMLDOMTool::GetElementAttributeDouble(CXMLDOMElement* pElement, CString strKey)
+{
+	COleVariant oVariant = pElement->getAttribute(strKey);
+	oVariant.ChangeType(VT_R8);
+	double dData = oVariant.dblVal;
+	return dData;
+}
+
+// 字符串转换为整数
+int CXMLDOMTool::ConvertStringToInt(CString strData)
+{
+	COleVariant oVariant = strData;
+	oVariant.ChangeType(VT_INT);
+	int iData = oVariant.intVal;
+	return iData;
+}
+
+// 字符串转换为无符号整数
+unsigned int CXMLDOMTool::ConvertStringToUnsignedInt(CString strData)
+{
+	COleVariant oVariant = strData;
+	oVariant.ChangeType(VT_UINT);
+	unsigned int uiData = oVariant.uintVal;
+	return uiData;
+}
+
+// 字符串转换为单精度浮点数
+float CXMLDOMTool::ConvertStringToFloat(CString strData)
+{
+	COleVariant oVariant = strData;
+	oVariant.ChangeType(VT_R4);
+	float fData = oVariant.fltVal;
+	return fData;
+}
+
+// 字符串转换为双精度浮点数
+double CXMLDOMTool::ConvertStringToDouble(CString strData)
+{
+	COleVariant oVariant = strData;
+	oVariant.ChangeType(VT_R8);
+	double dData = oVariant.dblVal;
+	return dData;
+}
