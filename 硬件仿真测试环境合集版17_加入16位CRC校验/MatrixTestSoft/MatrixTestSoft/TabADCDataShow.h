cd8a34f98ddf5163e74e29dd0b29b84999da24d6
chengxianming1981@gmail.com <chengxianming1981@gmail.com@5d2555b2-77f6-cb3c-1dbd-c1bb7278d601>
2011-05-27 01:40:08 +0000

    Work 硬件仿真测试环境合集版17_加入16位CRC校验
    git-svn-id: https://cxm-hitech-matrix428.googlecode.com/svn/trunk@80 5d2555b2-77f6-cb3c-1dbd-c1bb7278d601

---
+++ 硬件仿真测试环境合集版17_加入16位CRC校验/MatrixTestSoft/MatrixTestSoft/TabADCDataShow.h
@@ -0,0 +1,28 @@
+#pragma once
+
+// CTabADCDataShow 对话框
+
+class CTabADCDataShow : public CDialog
+{
+	DECLARE_DYNAMIC(CTabADCDataShow)
+
+public:
+	CTabADCDataShow(CWnd* pParent = NULL);   // 标准构造函数
+	virtual ~CTabADCDataShow();
+
+// 对话框数据
+	enum { IDD = IDD_DIALOG_ADCDATASHOW };
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+	virtual BOOL OnInitDialog();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnTimer(UINT_PTR nIDEvent);
+public:
+	// ADC数据界面显示缓冲区指针
+	double* m_pADCDataShow;
+
+	// 防止程序在循环中运行无法响应消息
+	void ProcessMessages(void);
+};
