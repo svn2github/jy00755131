19ff888bfe19b498ff7fb1fc453f67714161834c
chengxianming1981@gmail.com <chengxianming1981@gmail.com@5d2555b2-77f6-cb3c-1dbd-c1bb7278d601>
2011-05-27 07:34:26 +0000

    Work 端口监视器-开2个线程类_产生虚拟查询帧并对应答帧修改IP地址为虚拟设备IP_偏移量以74递增5
    git-svn-id: https://cxm-hitech-matrix428.googlecode.com/svn/trunk@125 5d2555b2-77f6-cb3c-1dbd-c1bb7278d601

---
+++ 端口监视器-开2个线程类_产生虚拟查询帧并对应答帧修改IP地址为虚拟设备IP_偏移量以74递增5/端口监视器/SendThread.h
@@ -0,0 +1,54 @@
+#pragma once
+
+
+// CSendThread
+
+class CSendThread : public CWinThread
+{
+
+	DECLARE_DYNCREATE(CSendThread)
+
+public:
+	CSendThread();           // 动态创建所使用的受保护的构造函数
+	virtual ~CSendThread();
+	virtual int Run();
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+protected:
+	DECLARE_MESSAGE_MAP()
+public:
+	CSocket m_hSocket2;
+	//CPortMonitoringDlg* m_pTDoc;
+	// 初始化
+	void Init();
+	bool m_close;
+	void OnClose();
+	unsigned char udp_buf[8][256];
+	unsigned char udp_count;
+	// ADC数据重发帧
+	unsigned char m_ucADCRetransmission[128];
+	// 需要重发的ADC数据信息结构
+	typedef struct m_structADC
+	{
+ 		unsigned short	uiRetransmissionNum;	// 接收到的重发计数
+		unsigned int	uiFrameCount;			// 程序转发ADC数据帧计数
+	};
+	// 需要重发的ADC索引表
+	CMap<unsigned int, unsigned int, m_structADC, m_structADC&> m_oADCLostMap;
+	// 设置ADC数据重发
+	void OnADCDataRetransmission(unsigned int uiIPAim, unsigned short usDataPointNb, unsigned short usPort);
+	// 生成ADC数据重发帧
+	void MakeADCDataRetransmissionFrame(unsigned int uiIPAim, unsigned short usDataPointNb, unsigned short usPort);
+	// 达到指定接收帧则清理过期的重发帧
+	unsigned int m_uiFrameTimeCount;
+	// 队列中的每个结构体的帧计数加一
+	void OnListCountAdd(void);
+	// 首个指针偏移量
+	unsigned short m_usDataPointPrevious;
+	// ADC接收帧计数
+	unsigned int m_uiADCFrameCount;
+};
+
+
---
+++ 端口监视器-开2个线程类_产生虚拟查询帧并对应答帧修改IP地址为虚拟设备IP_偏移量以74递增5/端口监视器/ThreadManage.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "RecThread.h"
+#include "SendThread.h"
+class CThreadManage
+{
+public:
+	CThreadManage(void);
+	~CThreadManage(void);
+
+public:
+	CRecThread m_RecThread;
+ 	CSendThread m_SendThread;
+	// ≥ı ºªØ
+	void Init();
+	void OnClose();
+};
