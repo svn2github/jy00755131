3a690cc44f857beb54d9412cb78919334d5a6bec
chengxianming1981@gmail.com <chengxianming1981@gmail.com@5d2555b2-77f6-cb3c-1dbd-c1bb7278d601>
2011-05-27 02:40:23 +0000

    Debug 硬件仿真测试环境合集版24_查询并打印硬件出错信息
    git-svn-id: https://cxm-hitech-matrix428.googlecode.com/svn/trunk@91 5d2555b2-77f6-cb3c-1dbd-c1bb7278d601

---
+++ 硬件仿真测试环境合集版24_查询并打印硬件出错信息_调试用/MatrixTestSoft/MatrixTestSoft/PortMonitoringRecThread.cpp
@@ -0,0 +1,204 @@
+// PortMonitoringRecThread.cpp : 实现文件
+//
+
+#include "stdafx.h"
+#include "MatrixTestSoft.h"
+#include "PortMonitoringRecThread.h"
+
+
+// CPortMonitoringRecThread
+
+IMPLEMENT_DYNCREATE(CPortMonitoringRecThread, CWinThread)
+
+CPortMonitoringRecThread::CPortMonitoringRecThread()
+: m_usudp_count(0)
+, m_iRecPort(0)
+, m_iSendPort(0)
+, m_csIP(_T(""))
+, m_pwnd(NULL)
+, m_pWndTab(NULL)
+, m_pSaveFile(NULL)
+, m_uiHeartBeatNum(0)
+, m_uiIPSetNum(0)
+, m_uiTailTimeNum(0)
+, m_uiDelayTimeSetNum(0)
+, m_uiADCSetNum(0)
+, m_uiSendFrameNum(0)
+, m_bclose(false)
+{
+}
+
+CPortMonitoringRecThread::~CPortMonitoringRecThread()
+{
+}
+
+BOOL CPortMonitoringRecThread::InitInstance()
+{
+	// TODO: 在此执行任意逐线程初始化
+	return TRUE;
+}
+
+int CPortMonitoringRecThread::ExitInstance()
+{
+	// TODO: 在此执行任意逐线程清理
+	return CWinThread::ExitInstance();
+}
+
+BEGIN_MESSAGE_MAP(CPortMonitoringRecThread, CWinThread)
+END_MESSAGE_MAP()
+
+
+// CPortMonitoringRecThread 消息处理程序
+
+int CPortMonitoringRecThread::Run()
+{
+	// TODO: 在此添加专用代码和/或调用基类
+
+	DWORD dwFrameCount = 0;
+	int icount = 0;
+	unsigned short uiPort = 0;
+	// 循环，直到关闭标志为真
+	while(true)
+	{
+		if(m_bclose == true)
+		{
+			break;
+		}
+	
+		// 得到网络接收缓冲区数据字节数
+		m_RecSocket.IOCtl(FIONREAD, &dwFrameCount);
+		dwFrameCount = dwFrameCount/SndFrameSize;
+		if(0 != dwFrameCount)	
+		{
+			for(unsigned int i = 0; i < dwFrameCount; i++)	// 依次检查每个仪器IP设置应答帧
+			{
+				ProcessMessages();
+				icount = m_RecSocket.Receive(m_ucudp_buf[m_usudp_count],SndFrameSize);
+				if (icount == SndFrameSize)
+				{ 
+					CString strTemp = _T("");
+					unsigned int uiCommand = 0;
+					// 注释掉下面的输出信息后在ADC参数设置和零漂过程中心跳就不会出现停顿现象
+					// 通过端口识别功能
+// 					memcpy(&uiPort, &m_ucudp_buf[m_usudp_count][24], 2);
+// 					if (uiPort == HeartBeatRec)
+// 					{
+// 						m_uiHeartBeatNum ++;
+// 						strTemp.Format(_T("%d"),m_uiHeartBeatNum);
+// 						m_pwnd->GetDlgItem(IDC_STATIC_HEARTBEATFRAMENUM)->SetWindowText(strTemp);
+// 					}
+// 					else if (uiPort == IPSetPort)
+// 					{
+// 						m_uiIPSetNum ++;
+// 						strTemp.Format(_T("%d"),m_uiIPSetNum);
+// 						m_pwnd->GetDlgItem(IDC_STATIC_IPSETNUM)->SetWindowText(strTemp);
+// 					}
+// 					else if (uiPort == TailTimeFramePort)
+// 					{
+// 						m_uiTailTimeNum ++;
+// 						strTemp.Format(_T("%d"),m_uiTailTimeNum);
+// 						m_pwnd->GetDlgItem(IDC_STATIC_TAILTIMESETNUM)->SetWindowText(strTemp);
+// 					}
+// 					else if (uiPort == TimeSetPort)
+// 					{
+// 						m_uiDelayTimeSetNum ++;
+// 						strTemp.Format(_T("%d"),m_uiDelayTimeSetNum);
+// 						m_pwnd->GetDlgItem(IDC_STATIC_DELAYTIMESETNUM)->SetWindowText(strTemp);
+// 					}
+// 					else if (uiPort == ADSetReturnPort)
+// 					{
+// 						m_uiADCSetNum ++;
+// 						strTemp.Format(_T("%d"),m_uiADCSetNum);
+// 						m_pwnd->GetDlgItem(IDC_STATIC_ADCSETNUM)->SetWindowText(strTemp);
+// 					}
+
+					m_uiSendFrameNum ++;
+// 					strTemp.Format(_T("%d"),m_uiSendFrameNum);
+// 					m_pWndTab->GetDlgItem(IDC_STATIC_SENDFRAMENUM)->SetWindowText(strTemp);
+
+					m_RecSocket.SendTo(m_ucudp_buf[m_usudp_count],SndFrameSize,m_iSendPort,m_csIP);
+					m_pSaveFile->OnSaveReceiveData(m_ucudp_buf[m_usudp_count],SndFrameSize);
+
+					m_usudp_count +=1;
+					m_usudp_count = m_usudp_count%8;
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+		if (m_bclose == true)
+		{
+			break;
+		}
+		Sleep(1);
+	}
+
+	::SetEvent(m_hPortMonitoringRecThreadClose);	// 设置事件对象为有信号状态,释放等待线程后将事件置为无信号
+	return CWinThread::Run();
+}
+
+// 初始化
+void CPortMonitoringRecThread::OnInit(void)
+{
+	m_hPortMonitoringRecThreadClose = ::CreateEvent(false, false, NULL, NULL);	// 创建事件对象
+	::ResetEvent(m_hPortMonitoringRecThreadClose);	// 设置事件对象为无信号状态
+}
+
+// 打开
+void CPortMonitoringRecThread::OnOpen(void)
+{
+	m_RecSocket.Close();
+	int flag =  m_RecSocket.Create(m_iRecPort,SOCK_DGRAM);
+	m_RecSocket.SetSockOpt(SO_RCVBUF,&m_RecSocket,PortMonitoringBufSize,SOL_SOCKET);
+	m_RecSocket.SetSockOpt(SO_SNDBUF,&m_RecSocket,PortMonitoringBufSize,SOL_SOCKET);
+}
+
+// 停止
+void CPortMonitoringRecThread::OnStop(void)
+{
+	m_RecSocket.Close();
+}
+
+// 关闭
+void CPortMonitoringRecThread::OnClose(void)
+{
+	m_RecSocket.Close();
+	m_bclose = true;
+}
+// 重置界面
+void CPortMonitoringRecThread::OnReset(void)
+{
+	CString strTemp;
+	m_uiHeartBeatNum = 0;
+	strTemp.Format(_T("%d"),m_uiHeartBeatNum);
+	m_pwnd->GetDlgItem(IDC_STATIC_HEARTBEATFRAMENUM)->SetWindowText(strTemp);
+
+	m_uiIPSetNum = 0;
+	strTemp.Format(_T("%d"),m_uiIPSetNum);
+	m_pwnd->GetDlgItem(IDC_STATIC_IPSETNUM)->SetWindowText(strTemp);
+
+	m_uiTailTimeNum = 0;
+	strTemp.Format(_T("%d"),m_uiTailTimeNum);
+	m_pwnd->GetDlgItem(IDC_STATIC_TAILTIMESETNUM)->SetWindowText(strTemp);
+
+	m_uiDelayTimeSetNum = 0;
+	strTemp.Format(_T("%d"),m_uiDelayTimeSetNum);
+	m_pwnd->GetDlgItem(IDC_STATIC_DELAYTIMESETNUM)->SetWindowText(strTemp);
+
+	m_uiADCSetNum = 0;
+	strTemp.Format(_T("%d"),m_uiADCSetNum);
+	m_pwnd->GetDlgItem(IDC_STATIC_ADCSETNUM)->SetWindowText(strTemp);
+
+	m_uiSendFrameNum = 0;
+	strTemp.Format(_T("%d"),m_uiSendFrameNum);
+	m_pWndTab->GetDlgItem(IDC_STATIC_SENDFRAMENUM)->SetWindowText(strTemp);
+}
+// 防止程序在循环中运行无法响应消息
+void CPortMonitoringRecThread::ProcessMessages(void)
+{
+	MSG msg;
+	::PeekMessage(&msg, NULL, 0, 0, PM_REMOVE);
+	::DispatchMessage(&msg);
+}
---
+++ 硬件仿真测试环境合集版24_查询并打印硬件出错信息_调试用/MatrixTestSoft/MatrixTestSoft/PortMonitoringRecThread.h
@@ -0,0 +1,75 @@
+#pragma once
+#include "Parameter.h"
+#include "SaveFile.h"
+
+
+// CPortMonitoringRecThread
+
+class CPortMonitoringRecThread : public CWinThread
+{
+	DECLARE_DYNCREATE(CPortMonitoringRecThread)
+
+public:
+	CPortMonitoringRecThread();           // 动态创建所使用的受保护的构造函数
+	virtual ~CPortMonitoringRecThread();
+
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+protected:
+	DECLARE_MESSAGE_MAP()
+
+protected:
+	// 数据接收缓冲
+	unsigned char m_ucudp_buf[8][SndFrameSize];
+	// 数据接收缓冲区计数
+	unsigned short m_usudp_count;
+	// 心跳计数
+	unsigned int m_uiHeartBeatNum;
+	// IP地址设置计数
+	unsigned int m_uiIPSetNum;
+	// 尾包时刻查询计数
+	unsigned int m_uiTailTimeNum;
+	// 时延设置计数
+	unsigned int m_uiDelayTimeSetNum;
+	// ADC设置计数
+	unsigned int m_uiADCSetNum;
+public:
+	// 接收方Socket
+	CSocket m_RecSocket;
+	// 接收端口
+	int m_iRecPort;
+	// 发送端口
+	int m_iSendPort;
+	// 目标IP地址
+	CString m_csIP;
+	// 界面PortMonitoringSend指针
+	CWnd* m_pwnd;
+	// 界面TabPortMonitoring指针
+	CWnd* m_pWndTab;
+	// 保存文件类的指针
+	CSaveFile* m_pSaveFile;
+	// 线程关闭标志
+	bool m_bclose;
+	// 线程结束事件
+	HANDLE m_hPortMonitoringRecThreadClose;
+	// 发送帧数
+	unsigned int m_uiSendFrameNum;
+public:
+	virtual int Run();
+	// 初始化
+	void OnInit(void);
+	// 打开
+	void OnOpen(void);
+	// 停止
+	void OnStop(void);
+	// 关闭
+	void OnClose(void);
+	// 防止程序在循环中运行无法响应消息
+	void ProcessMessages(void);
+	// 重置界面
+	void OnReset(void);
+};
+
+
