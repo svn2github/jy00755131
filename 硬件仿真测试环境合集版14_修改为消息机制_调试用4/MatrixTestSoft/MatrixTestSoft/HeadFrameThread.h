50b9cfbb194e15d1ceb37b32f0e283c1c70f58af
chengxianming1981@gmail.com <chengxianming1981@gmail.com@5d2555b2-77f6-cb3c-1dbd-c1bb7278d601>
2011-05-27 01:17:57 +0000

    Debug 硬件仿真测试环境合集版14_4_修改为消息机制
    git-svn-id: https://cxm-hitech-matrix428.googlecode.com/svn/trunk@77 5d2555b2-77f6-cb3c-1dbd-c1bb7278d601

---
+++ 硬件仿真测试环境合集版14_修改为消息机制_调试用4/MatrixTestSoft/MatrixTestSoft/HeadFrameThread.h
@@ -0,0 +1,76 @@
+#pragma once
+#include "InstrumentList.h"
+
+
+// CHeadFrameThread
+
+class CHeadFrameThread : public CWinThread
+{
+	DECLARE_DYNCREATE(CHeadFrameThread)
+
+public:
+	CHeadFrameThread();           // 动态创建所使用的受保护的构造函数
+	virtual ~CHeadFrameThread();
+	virtual int Run();
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+protected:
+	DECLARE_MESSAGE_MAP()
+public:
+	// 初始化
+	void OnInit(void);
+	// 关闭并结束线程
+	void OnClose(void);
+	// 关闭线程标志位
+	bool m_bclose;
+	// 线程结束事件
+	HANDLE m_hHeadFrameThreadClose;
+	// 首包接收Socket
+	CSocket m_HeadFrameSocket;
+	// IP地址设置Socket
+	CSocket m_IPSetSocket;
+	// 处理单个首包
+	void ProcHeadFrameOne();
+	// 解析首包
+	BOOL ParseHeadFrame(void);
+	// 仪器串号
+	DWORD m_uiSN;
+	// 首包时刻
+	DWORD m_uiHeadFrameTime;
+	// 路由地址
+	DWORD m_uiRoutAddress;
+	// 仪器的IP地址
+	DWORD m_uiIPAddress;
+	// 仪器列表指针
+	CInstrumentList* m_oInstrumentList;
+	// 生成IP地址设置帧
+	void MakeIPSetFrame(CInstrument* pInstrument);
+	// 首包接收数据缓冲
+	byte m_pHeadFrameData[256];
+	// 设置IP地址缓冲
+	byte m_pIPSetFrameData[128];
+	// IP地址设置应答缓冲
+	byte m_pIPSetReturnFrameData[256];
+	// 发送IP地址设置帧
+	void SendIPSetFrame(void);
+	// 单个IP地址设置应答帧处理
+	void ProcIPSetReturnFrameOne(void);
+	// 解析IP地址设置应答帧
+	BOOL ParseIPSetReturnFrame(void);
+	// 打开
+	void OnOpen(void);
+	// 停止
+	void OnStop(void);
+	// 源IP地址
+	CString m_csIPSource;
+	// 发送端口
+	unsigned int m_uiSendPort;
+	// 设备连接上的图标指针
+	HICON m_iconConnected;
+	// 界面指针
+	CWnd* m_pwnd;
+};
+
+
