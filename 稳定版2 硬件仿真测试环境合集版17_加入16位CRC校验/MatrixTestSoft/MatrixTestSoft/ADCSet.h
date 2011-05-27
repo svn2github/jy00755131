f892a6449e908ea473ed8c92e6680a9692f7e179
chengxianming1981@gmail.com <chengxianming1981@gmail.com@5d2555b2-77f6-cb3c-1dbd-c1bb7278d601>
2011-05-27 02:13:21 +0000

    Milestone 硬件仿真测试环境合集版17_加入16位CRC校验
    git-svn-id: https://cxm-hitech-matrix428.googlecode.com/svn/trunk@85 5d2555b2-77f6-cb3c-1dbd-c1bb7278d601

---
+++ 稳定版2 硬件仿真测试环境合集版17_加入16位CRC校验/MatrixTestSoft/MatrixTestSoft/ADCSet.h
@@ -0,0 +1,54 @@
+#pragma once
+#include "TabADCSettings.h"
+#include "Parameter.h"
+class CADCSet
+{
+public:
+	CADCSet(void);
+	~CADCSet(void);
+public:
+	// ADC设置帧
+	unsigned char m_ucFrameData[SndFrameSize];
+	// ADC设置帧头
+	void ADCSetFrameHead(unsigned int uiIPAim, bool bBroadCast, unsigned short	usCommand, unsigned short usPortAim);
+	// 发送ADC设置的18指令
+	int ADCCommand_18(int iPos, byte * cADCSet, unsigned int uiLength);
+	// 设置正弦输出
+	void OnSetSine(void);
+	// 设置停止采集
+	void OnStopSample(void);
+	// 打开电源
+	void OnOpenPowerTBLow(void);
+	// 打开TB电源
+	void OnOpenPowerTBHigh(void);
+	// 打开开关
+	void OnOpenSwitchTBLow(void);
+	// 打开TB开关
+	void OnOpenSwitchTBHigh(void);
+	// 读ADC寄存器
+	void OnADCRegisterRead(void);
+	// 写ADC寄存器
+	void OnADCRegisterWrite(void);
+	// ADC设置参数返回
+	void OnADCSetReturn(void);
+	// ADC连续读取数据
+	void OnADCReadContinuous(bool bBroadCast);
+	// 设置TB时间
+	void OnSetTB(unsigned int tnow, bool bSwitch);
+	// 打开零漂矫正电源及开关
+	void OnOpenPowerZeroDrift(void);
+	// 查询ADC寄存器
+	void OnADCRegisterQuery(void);
+	// ADC数据采集同步
+	void OnADCSampleSynchronization(void);
+	// ADC零漂偏移矫正
+	void OnADCZeroDriftCorrection(void);
+	// ADC零漂设置（按IP地址）
+	void OnADCZeroDriftSetFromIP(unsigned char* ucZeroDrift);
+	// ADC参数设置选择界面指针
+	CTabADCSettings* m_pTabADCSettings;
+	// 计算CRC校验值
+	void OnCRCCal(void);
+	// 打开TB开关
+	void OnSetTBSwitchOpen(void);
+};
