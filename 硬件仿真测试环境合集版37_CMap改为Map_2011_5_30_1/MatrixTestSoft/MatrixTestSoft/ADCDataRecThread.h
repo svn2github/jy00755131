822697087b5074119b44f0ba7eaa93ac01a6162f
chengxianming1981@gmail.com <chengxianming1981@gmail.com@5d2555b2-77f6-cb3c-1dbd-c1bb7278d601>
2011-09-08 08:06:24 +0000

    Work 硬件仿真测试环境合集版37_2011_9_8_2_x_5:(1)整理了CADCDataRecThread类。（2）可以正常ADC数据采集及绘图。
    git-svn-id: https://cxm-hitech-matrix428.googlecode.com/svn/trunk@203 5d2555b2-77f6-cb3c-1dbd-c1bb7278d601

--- 硬件仿真测试环境合集版37_CMap改为Map_2011_5_30_1/MatrixTestSoft/MatrixTestSoft/ADCDataRecThread.h
+++ 硬件仿真测试环境合集版37_CMap改为Map_2011_5_30_1/MatrixTestSoft/MatrixTestSoft/ADCDataRecThread.h
@@ -39,22 +39,38 @@ public:
 	SOCKET m_ADCDataSocket;
 	// ADC数据图形化显示Socket套接字
 	SOCKET m_ADCGraphShowSocket;
-	// 线程关闭标志
-	bool m_bclose;
 	// 图形模板指针
 	COScopeCtrl* m_pOScopeCtrl[InstrumentNum];
-	// 记录前一个接收到的ADC数据的指针偏移量                  
-	unsigned short m_usDataPointPrevious[InstrumentNum];
 	// 记录应接收ADC数据包个数
 	unsigned int m_uiADCDataFrameCount[InstrumentNum];
-	// 记录实际接收ADC数据包个数
-	unsigned int m_uiADCDataFrameRecCount[InstrumentNum];
 	// 判断ADC采样开始标志位，用于丢弃TB之前的无效数据
 	BOOL m_bStartSample;
-	// ADC数据重发帧
-	unsigned char m_ucADCRetransmission[SndFrameSize];
 	// 源IP地址
 	unsigned int m_uiIPSource;
+	// ADC图形显示设备IP
+	unsigned int m_uiADCGraphIP[InstrumentNum];
+	// ADC数据界面显示缓冲区
+	unsigned int m_uiADCDataShow[InstrumentNum];
+	// 类CADCDataSaveToFile成员变量指针
+	CADCDataSaveToFile* m_pADCDataSaveToFile;
+	// 类CADCFrameInfo成员变量指针
+	CADCFrameInfo* m_pADCFrameInfo;
+	// 日志类指针
+	CLogFile* m_pLogFile;
+	// ADC数据采样采样率设置
+	unsigned int m_uispsSelect;
+	// 设备类指针
+	CInstrumentList* m_pInstrumentList;
+
+private:
+	// 线程关闭标志
+	bool m_bclose;
+	// 记录前一个接收到的ADC数据的指针偏移量                  
+	unsigned short m_usDataPointPrevious[InstrumentNum];
+	// 记录实际接收ADC数据包个数
+	unsigned int m_uiADCDataFrameRecCount[InstrumentNum];
+	// ADC数据重发帧
+	unsigned char m_ucADCRetransmission[SndFrameSize];
 	// 需要重发的ADC数据信息结构
 	typedef struct m_structADC
 	{
@@ -75,10 +91,6 @@ public:
 	int m_iTemp[ADCDataTempDataSize];
 	// 测试丢帧情况
 	unsigned int m_uiTestADCLost;
-	// ADC图形显示设备IP
-	unsigned int m_uiADCGraphIP[InstrumentNum];
-	// ADC数据界面显示缓冲区
-	unsigned int m_uiADCDataShow[InstrumentNum];
 	// ADC接收到的数据重发帧的序号
 	unsigned int m_uiADCRetransmissionNb;
 	// 数据帧接收缓冲
@@ -89,36 +101,31 @@ public:
 	unsigned int m_uiUdpCount;
 	// UDP接收缓冲区
 	unsigned char m_ucUdpBuf[ADCDataBufSize];
-	// 类CADCDataSaveToFile成员变量指针
-	CADCDataSaveToFile* m_pADCDataSaveToFile;
-	// 类CADCFrameInfo成员变量指针
-	CADCFrameInfo* m_pADCFrameInfo;
-	// 日志类指针
-	CLogFile* m_pLogFile;
-	// ADC数据采样采样率设置
-	unsigned int m_uispsSelect;
-	// 设备类指针
-	CInstrumentList* m_pInstrumentList;
 	// ADC数据图形化显示设置接收缓冲区
 	byte m_pADCGraphSetFrameData[RcvFrameSize];
 	// ADC数据图形化显示抽样率
 	unsigned int m_uiSamplingRate;
-protected:
-	// 被选择仪器的数值比较
-	int OnADCRecDataCheckOpt(unsigned short usOperation, unsigned int* pData);
-public:
-	// 初始化
-	void OnInit(void);
-	// 关闭并结束线程
-	void OnClose(void);
+	// 开始ADC数据图形化显示标志位
+	BOOL m_bStartGraphShow;
+	// 监测参与采样的设备应收ADC数据包个数是否一致
+	BOOL m_bCheckADCDataFrameCountEqule;
+	// ADC数据图形化显示设置缓冲区
+	unsigned char m_ucADCGraphShowSetFrame[RcvFrameSize];
+	// ADC数据图形化显示数据缓冲区
+	unsigned char m_ucADCGraphShowDataFrame[InstrumentNum][RcvFrameSize];
+	// ADC数据图形化显示抽样点数
+	unsigned int m_uiADCGraphShowSamplingNum[InstrumentNum];
+	// ADC数据图形化显示数据源指针坐标
+	unsigned int m_uiADCGraphShowSourcePoint[InstrumentNum];
+private:
 	// 接收采样数据
 	void ReceiveSampleData(byte* pSampleData);
+	// 被选择仪器的数值比较
+	int OnADCRecDataCheckOpt(unsigned short usOperation, unsigned int* pData);
 	// 生成ADC数据重发帧
 	void MakeADCDataRetransmissionFrame(unsigned short usDataPointNb, unsigned int uiIPAim);
 	// ADC数据需要重新发送帧对应的最小个数
 	unsigned int OnADCRetransimissionMinNb(unsigned int uiInstrumentNb);
-	// 将ADC数据保存到文件中
-	void OnSaveADCToFile(bool bfinish);
 	// ADC数据重发
 	void OnADCDataRetransmission(void);
 	// ADC数据重发
@@ -135,31 +142,25 @@ public:
 	void OnRecNotOkIsRetransimissionFrame(unsigned int uiInstrumentNb, unsigned int uiIPAim, int* piReceiveData);
 	// 该帧为普通帧且之前有重发帧
 	void OnRecNotOkIsNormalFrame(unsigned int uiInstrumentNb, int* piReceiveData);
-	// 重置变量的值
-	void OnReset(void);
 	// 数据处理
 	void OnProcess(int iCount);
-	// 关闭UDP套接字
-	void OnCloseUDP(void);
 	// 生成ADC数据图形化显示帧
 	void OnMakeADCGraphShowFrame(unsigned short usCommand);
-	// ADC数据图形化显示设置接收函数
-	void OnReceive(void);
 	// ADC图形化显示设置帧处理函数
 	void ProcADCGraphSetFrame(void);
-private:
-	// 开始ADC数据图形化显示标志位
-	BOOL m_bStartGraphShow;
-	// 监测参与采样的设备应收ADC数据包个数是否一致
-	BOOL m_bCheckADCDataFrameCountEqule;
-	// ADC数据图形化显示设置缓冲区
-	unsigned char m_ucADCGraphShowSetFrame[RcvFrameSize];
-	// ADC数据图形化显示数据缓冲区
-	unsigned char m_ucADCGraphShowDataFrame[InstrumentNum][RcvFrameSize];
-	// ADC数据图形化显示抽样点数
-	unsigned int m_uiADCGraphShowSamplingNum[InstrumentNum];
-	// ADC数据图形化显示数据源指针坐标
-	unsigned int m_uiADCGraphShowSourcePoint[InstrumentNum];
+public:
+	// 初始化
+	void OnInit(void);
+	// 关闭并结束线程
+	void OnClose(void);
+	// 重置变量的值
+	void OnReset(void);
+	// 关闭UDP套接字
+	void OnCloseUDP(void);
+	// ADC数据图形化显示设置接收函数
+	void OnReceive(void);
+	// 将ADC数据保存到文件中
+	void OnSaveADCToFile(bool bfinish);
 };
 
 
