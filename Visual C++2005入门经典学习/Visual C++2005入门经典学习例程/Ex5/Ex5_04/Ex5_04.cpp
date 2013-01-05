f3c50d9ab53a3ae81dba9f568bc270ec30510219
chengxianming1981@gmail.com <chengxianming1981@gmail.com@5d2555b2-77f6-cb3c-1dbd-c1bb7278d601>
2013-01-05 09:07:45 +0000

    （1）加入第五章学习例程
    git-svn-id: https://cxm-hitech-matrix428.googlecode.com/svn/trunk@523 5d2555b2-77f6-cb3c-1dbd-c1bb7278d601

---
+++ Visual C++2005入门经典学习/Visual C++2005入门经典学习例程/Ex5/Ex5_04/Ex5_04.cpp
@@ -0,0 +1,27 @@
+/********************************************************
+*	Ex5_04.cpp											*
+*	Passing an array to a function						*
+********************************************************/
+#include <iostream>
+using std::cout;
+using std::endl;
+double average(double array[], int count);
+int main(void)
+{
+	double values[] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0};
+	cout << endl
+		<< "Average = "
+		<< average(values, (sizeof values)/ (sizeof values[0]));
+
+	cout << endl;
+	return 0;
+}
+double average(double array[], int count)
+{
+	double sum = 0.0;
+	for (int i=0; i<count; i++)
+	{
+		sum += array[i];
+	}
+	return sum/count;
+}
---
+++ Visual C++2005入门经典学习/Visual C++2005入门经典学习例程/Ex5/Ex5_06/Ex5_06.cpp
@@ -0,0 +1,10 @@
+/********************************************************
+*	Ex5_06.cpp											*
+*	Passing a two-dimensional array to a function		*
+********************************************************/
+#include <iostream>
+
+int main(void)
+{
+	return 0;
+}
